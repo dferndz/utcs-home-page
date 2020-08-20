8400cfeaafa4b615d70bfd333d9ef27e174dde01
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2020-08-20 11:48:35 -0500

    basic app setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { pages, Page } from "../pages";
+
+import Navbar from "../components/Navbar";
+
+import "../assets/css/bootstrap.min.css";
 
 const App = () => {
-  return <p>Hello</p>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        {pages.map((page: Page, key: number) => (
+          <Route
+            exact={page.exact}
+            path={page.path}
+            component={page.component}
+            key={key}
+          />
+        ))}
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages.ts
@@ -0,0 +1,17 @@
+import HomePage from "./features/Home";
+
+export type Page = {
+  title: string;
+  path: string;
+  component: any;
+  exact: boolean;
+};
+
+export const pages: Page[] = [
+  {
+    title: "Home page",
+    path: "/",
+    component: HomePage,
+    exact: true,
+  },
+];
