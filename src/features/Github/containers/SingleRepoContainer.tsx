033766ed1e7a0eeee35d669681836c19eab8d121
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2020-08-21 10:12:52 -0500

    fix single repo page

--- src/features/Github/containers/SingleRepoContainer.tsx
+++ src/features/Github/containers/SingleRepoContainer.tsx
@@ -2,30 +2,28 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import RepoPage from "../components/RepoPage";
 import { useParams } from "react-router-dom";
-import { GITHUB_USERNAME } from "../../../constants";
+import { useCv } from "../../../context";
 
 type Params = {
   repo: string;
 };
 
 const SingleRepoContainer = () => {
+  const cv = useCv();
   const { repo } = useParams<Params>();
 
-  if (!GITHUB_USERNAME)
+  if (!cv.profile?.github)
     return (
       <Container className="mt-4">
         <Row>
           <Col md={12} className="text-center">
-            <p>
-              Set the environment valirable
-              REACT_APP_GITHUB_USERNAME=your_github_username
-            </p>
+            <p>To connect to Github add a github username to your profile.</p>
           </Col>
         </Row>
       </Container>
     );
 
-  return <RepoPage user={GITHUB_USERNAME} repo={repo} />;
+  return <RepoPage user={cv.profile.github} repo={repo} />;
 };
 
 export default SingleRepoContainer;
